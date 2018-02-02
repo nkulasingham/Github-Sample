6163198aa8988b879d686be9d45237a39d91d192
Nilushanan Kulasingham <nkulasingham@gmail.com>
2018-02-03 00:10:20 +1100

    add NotFound component

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './styles.scss';
+
+const NotFound = props => (
+  <div className={styles.container}>
+    <h1 className={styles.title}> Gitgazers - 404 </h1>
+    <h className={styles.totalResults}>Uh oh, looks like you&apos;ve taken a wrong turn .</h>
+    <div className={styles.flexContainer}>
+      <button onClick={props.history.goBack} to="/" className={styles.button}>
+        Click here to go back
+      </button>
+    </div>
+  </div>
+);
+
+export default NotFound;
